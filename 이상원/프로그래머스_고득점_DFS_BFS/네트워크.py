87e4953b56a00fb8f9ece7e901fa8118961a8694
knowmetoowell <87797445+knowmetoowell@users.noreply.github.com>
2023-10-31 23:17:24 +0900

    231031

--- 이상원/프로그래머스_고득점_DFS_BFS/네트워크.py
+++ 이상원/프로그래머스_고득점_DFS_BFS/네트워크.py
@@ -1,29 +1,29 @@
 from collections import deque
 def solution(n, computers):
     answer = 0
-    queue = deque()
-    
-    
-    connected = [False]*n
-    connected[0] = True
-    queue.append(n[0])
-
-    while queue:
-        pop = queue.popleft()
-        for i in range(1, len(a)):
-            if connected[i] == False:
-                if pop[i] == 1:
-                    connected[i] = True
-                    queue.append(n[i])
-                
-
-            
-
-
-
-
+    visited = [0 for i in range(n)] 
+    def bfs(pop):
+        queue = deque()
+        queue.append(pop)
+        while queue:
+            pop = queue.popleft()
+            visited[pop] = 1
+            for m in range(n):
+                if computers[pop][m]==1 and visited[m] == 0:
+                    queue.append(m)
+    for i in range(n):
+        if visited[i] == 0:
+            bfs(i)
+            answer+=1
+        
 
     return answer
 
-connected = [False]*5
-print(connected)
+print(solution(3,[[1, 1, 0], [1, 1, 1], [0, 1, 1]]))
+
+
+if 1:
+    print("ooo")
+
+if 0:
+    print("ooooooooo")
