e2a76bcf9400aef3ecd782f94e9e6fb5ba43d31a
knowmetoowell <87797445+knowmetoowell@users.noreply.github.com>
2023-11-03 11:13:54 +0900

    231103 - 미완성

--- 이상원/프로그래머스_고득점_DFS_BFS/아이템 줍기_lv3.py
+++ 이상원/프로그래머스_고득점_DFS_BFS/아이템 줍기_lv3.py
@@ -45,13 +45,35 @@ def xx(rectangle, characterX, characterY, itemX, itemY):
             for j in range(y1, y2+1):
             	# x1, x2, y1, y2는 테두리이므로 제외하고 내부만 0으로 채움
                 if x1 < i < x2 and y1 < j < y2:
-                    field[i][j] = 0
+                    field[i][j] = -2
                 # 다른 직사각형의 내부가 아니면서 테두리일 때 1로 채움
                 elif field[i][j] != 0:
-                    field[i][j] = 1
+                    field[i][j] = 0
 
     cx2, cy2, ix2, iy2 = characterX*2, characterY*2, itemX*2,itemY*2
-    return field
+
+    q = deque()
+    q.append([0,cx2,cy2])
+    dx =[-1,1,0,0]
+    dy =[0,0,-1,1]
+    n = len(field) # x
+    m = len(field[0]) # y
+    
+    while q:
+        ans, x, y = q.popleft()
+        if x == ix2 and y == iy2:
+            answer = ans
+            break
+        
+        field[x][y] = ans + 1
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i] 
+            if 0<=nx< n and 0 <= ny < m and field[nx][ny] == 0:
+                q.append([ans+1, nx,ny])
+                
+
+    return answer/2
 
 
 
@@ -59,5 +81,6 @@ def xx(rectangle, characterX, characterY, itemX, itemY):
 
 lst = xx([[1,1,7,4],[3,2,5,5],[4,3,6,9],[2,6,8,8]],	1,	3,	7,	8)
 # # pprint(lst)
-
+a = solution([[1,1,7,4],[3,2,5,5],[4,3,6,9],[2,6,8,8]],	1,	3,	7,	8)
+print(lst)
 # print(*lst, sep='\n')
