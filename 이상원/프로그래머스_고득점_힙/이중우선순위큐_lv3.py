502b6516b9777d7ed48cad8fd12ef723a1050d9e
knowmetoowell <87797445+knowmetoowell@users.noreply.github.com>
2023-11-08 19:03:28 +0900

    231108 이중우선순위큐_lv3

---
+++ 이상원/프로그래머스_고득점_힙/이중우선순위큐_lv3.py
@@ -0,0 +1,46 @@
+import heapq
+
+def solution(operations):
+    
+    lst = []
+    lst2 = []
+    for op in operations:
+        sp = op.split(" ")
+        if sp[0] == "I":
+            heapq.heappush(lst, [int(sp[1]), -int(sp[1])])
+            # heapq.heappush(lst2, "-" + sp[1])
+        else:
+            if lst:
+                if sp[1] == "-1": #최소값 삭제
+                    heapq.heappop(lst)
+                
+                else: #최댓값 삭제               
+                    lst = change_heap(lst)
+                    heapq.heappop(lst)
+                    lst = change_heap(lst)
+                
+                    # heapq.heappop(lst2)    
+    print(lst)
+    answer = [0]*2
+    if len(lst) >1:
+        answer[1] = heapq.heappop(lst)[0]
+        answer[0] = heapq.heappop(change_heap(lst))[1]
+    elif len(lst) == 1:
+        answer[0], answer[1] = heapq.heappop(lst)[0]
+    elif len(lst) == 0:
+        return answer
+    
+    return answer
+
+def change_heap(lst):
+    temp = len(lst)
+    lst2 = []
+    if lst:
+        for _ in range(temp):
+            pop = heapq.heappop(lst)
+            heapq.heappush(lst2, [pop[1], pop[0]])
+    return lst2
+
+
+lst = ["I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333"]
+print(solution(lst))
