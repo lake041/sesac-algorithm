6672c452a4b89a92ea5a87c687a9ee550cc3765a
Choi-Seong-Hyeok <cinemay3@daum.net>
2023-11-10 17:56:10 +0900

    [Programmers] 11/10 solved..

---
+++ 최성혁/프로그래머스/정렬/가장 큰 수.py
@@ -0,0 +1,14 @@
+def solution(numbers):
+    answer = ""
+    # 처음 생각 모든 경우를 다 만들어서 max값 반환(X)
+
+    # 숫자만 스트링으로 변환
+    for i in range(len(numbers)):
+        numbers[i] = str(numbers[i])
+
+    # 1000 이하의 값이므로 3자리수 비교를위해 *3
+    numbers = sorted(numbers, reverse=True, key=lambda x: x * 3)
+
+    answer = answer.join(numbers)
+
+    return str(int(answer))
