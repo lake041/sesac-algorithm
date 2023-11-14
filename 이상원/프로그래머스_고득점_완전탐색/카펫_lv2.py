da11a07515a4f3ee5fe04e75cabb9b3425100335
knowmetoowell <87797445+knowmetoowell@users.noreply.github.com>
2023-11-14 20:32:20 +0900

    231114 카펫_lv2
    수학적인 사고력이 필요했던 문제

--- 이상원/프로그래머스_고득점_완전탐색/카펫_lv2.py
+++ 이상원/프로그래머스_고득점_완전탐색/카펫_lv2.py
@@ -23,18 +23,28 @@ def solution2(brown, yellow):
 
     return anslst[i]
 
+
+#a>=b
+#2a+2b - 4 = brown
+#(a-2)*(b-2) =yellow
+# => ab -2a -2b + 4 = yellow
+# => ab -brown = yellow
+# => ab = yellow + brown
 def solution(brown, yellow):
     answer = []
-    total = brown + yellow                  # a * b = total
-    for b in range(1,total+1):
-        if (total / b) % 1 == 0:            # total / b = a
-            a = total / b
+    ab = brown + yellow                  # a * b = ab
+    for b in range(1,ab+1):
+        if (ab / b) % 1 == 0:            # ab / b = a
+            a = ab / b
             if a >= b:                      # a >= b
                 if 2*a + 2*b == brown + 4:  # 2*a + 2*b = brown + 4 
                     return [a,b]
             
     return answer
-print(solution(10,2))
+
+
+
+print(solution(10,2))   
 
 print(solution(24,24))
 
