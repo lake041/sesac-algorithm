a87ee009d800dea7dc5f2c5ad76f1824261248ff
knowmetoowell <87797445+knowmetoowell@users.noreply.github.com>
2023-11-17 20:04:04 +0900

    231117 단속카메라_lv3

---
+++ 이상원/프로그래머스_고득점_탐욕법/단속카메라_lv3.py
@@ -0,0 +1,22 @@
+def solution(routes):
+    answer = 0
+    ans_lst = [routes.pop(0)]
+    while routes:
+        pop = routes.pop(0)
+
+        for i, ans in enumerate(ans_lst):
+            if (ans[0] <= pop[0] and pop[0] <= ans[1]) or (ans[0] <= pop[1] and pop[1] <= ans[1]):
+                lst = [ans[0],pop[0],ans[1], pop[1]]
+                lst.sort()
+                lst.pop(0)
+                lst.pop()
+                ans_lst[i] = lst
+                break
+            if i+1 == len(ans_lst):
+                ans_lst.append(pop)
+            
+
+
+    return len(ans_lst)
+
+print(solution([[-20,-15], [-14,-5], [-18,-13], [-5,-3]]))
