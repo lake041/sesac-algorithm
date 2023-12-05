b158fabbacb36d12ed5279161437406bf27e4e96
lake041 <lake041@korea.ac.kr>
2023-12-06 01:30:00 +0900

    231206: 백준 그리디 및 폴더 정리

--- 김민재/백준/그리디/보석 도둑.py
+++ 김민재/백준/그리디/보석 도둑.py
@@ -1,18 +1,16 @@
 # https://www.acmicpc.net/problem/1202
 
-from collections import deque
-from heapq import heapify, heappop, heappush
+from heapq import heappop, heappush
 
 N, K = map(int, input().split())
-dias = deque(sorted([list(map(int, input().split())) for _ in range(N)]))
+dias = sorted([list(map(int, input().split())) for _ in range(N)], reverse=True)
 bags = sorted([int(input()) for _ in range(K)])
-
 heapq = []
 
 ans = 0
 for bag in bags:
-    while dias and dias[0][0] <= bag:
-        weight, value = dias.popleft()
+    while dias and dias[-1][0] <= bag:
+        _, value = dias.pop()
         heappush(heapq, -value)
     ans += -heappop(heapq) if heapq else 0
 print(ans)
