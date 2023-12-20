f5979bab03d4cff38655de0f880a0b1f1e70ada4
knowmetoowell <87797445+knowmetoowell@users.noreply.github.com>
2023-12-20 18:36:23 +0900

    231220

--- 이상원/프로그래머스/8주차/혼자서하는틱택토_lv2.py
+++ 이상원/프로그래머스/8주차/혼자서하는틱택토_lv2.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 def solution(board):
-    answer = -1
+    answer = 1
     dic = defaultdict(int)
 
     for i,j,k in board:
@@ -10,41 +10,36 @@ def solution(board):
     
     numO = dic['O']
     numX = dic['X']
-    numDot = dic['.']
-    print(numO)
-    if numO < numX or numO >= numX+2:
-        return 0
-    if numDot == 9:
-        return 1
-
-    cor = [0,0]
-
-    if numO == numX and numO>=3:
-        for i,j,k in zip(board[0],board[1],board[2]):
-            if i==j==k=="O":
-                cor[0] = 1
-            if i==j==k=="X":
-                cor[1] = 1
-        for b in board:
-            if b[0]==b[1]==b[2]=="O":
-                cor[0] = 1
-            if b[0]==b[1]==b[2]=="X":
-                cor[1] = 1
-        if board[0][0]==board[1][1]==board[2][2]=="O":
-            cor[0] = 1
-        if board[0][0]==board[1][1]==board[2][2]=="X":
-            cor[1] = 1
-        if board[0][2]==board[1][1]==board[2][0]=="O":
-            cor[0] = 1
-        if board[0][2]==board[1][1]==board[2][0]=="X":
-            cor[0] = 1
-
-        if cor[0] == 1:
+    # numDot = dic['.']
+    if not (numO == numX or numO == numX + 1):
             return 0
-        if cor[1] == 1:
-            return 1
-        if cor[0]==cor[1]==0:
-            return 1
+    win = [0,0]
+    for i,j,k in zip(board[0],board[1],board[2]):
+        if i==j==k=="O":
+            win[0] = 1
+        if i==j==k=="X":
+            win[1] = 1
+    for b in board:
+        if b[0]==b[1]==b[2]=="O":
+            win[0] = 1
+        if b[0]==b[1]==b[2]=="X":
+            win[1] = 1
+    if board[0][0]==board[1][1]==board[2][2]=="O":
+        win[0] = 1
+    if board[0][0]==board[1][1]==board[2][2]=="X":
+        win[1] = 1
+    if board[0][2]==board[1][1]==board[2][0]=="O":
+        win[0] = 1
+    if board[0][2]==board[1][1]==board[2][0]=="X":
+        win[1] = 1
+    if win[0] == win[1] == 1:
+        return 0
+    if win[0] == 1 and numO != numX + 1:
+        return 0
+    if win[1] == 1 and numO != numX:
+        return 0
+    # if cor[0]==cor[1]==0:
+    #     return 1
 
 
     return 1
@@ -52,7 +47,7 @@ solution(["...", "...", "..."])
 # O 갯수 < X 갯수 return 0
 # O 갯수 >=  X 갯수+2 return 0
 # . 갯수 == 9 return 1
-# if  O 갯수 == X 갯수 
+# 
     # if O성공
         # return 0
     # elif X 성공
