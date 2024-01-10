03384342f339a61a535a968d9090ccce7d7d6577
knowmetoowell <87797445+knowmetoowell@users.noreply.github.com>
2024-01-10 16:37:06 +0900

    Update 큰수만들기_lv2.py

--- 이상원/프로그래머스_고득점_탐욕법/큰수만들기_lv2.py
+++ 이상원/프로그래머스_고득점_탐욕법/큰수만들기_lv2.py
@@ -1,51 +1,51 @@
-from itertools import product, permutations, combinations
-def solution(number, k):
-    len_ans = len(number)-k
-    answer = "1"
-    first = 0
-    sec = 0
-    ans = ""
-    while 1:
+# from itertools import product, permutations, combinations
+# def solution(number, k):
+#     len_ans = len(number)-k
+#     answer = "1"
+#     first = 0
+#     sec = 0
+#     ans = ""
+#     while 1:
         
-        for i in range(len(number)):
+#         for i in range(len(number)):
             
-            if (len_ans==len(number)-i):
-                break
-            if answer < number[i]:
-                answer = number[i]
-                first = i+1
+#             if (len_ans==len(number)-i):
+#                 break
+#             if answer < number[i]:
+#                 answer = number[i]
+#                 first = i+1
             
-        number = number[first:]
-        ans += answer
-        if len(number) == len_ans- len(ans):
-            ans += number
-        else:
-            len_ans = len_ans- len(ans)
-        answer="1"
-        # 3234
-        if len(ans) == len_ans:
-            break
+#         number = number[first:]
+#         ans += answer
+#         if len(number) == len_ans- len(ans):
+#             ans += number
+#         else:
+#             len_ans = len_ans- len(ans)
+#         answer="1"
+#         # 3234
+#         if len(ans) == len_ans:
+#             break
 
-    return ans
+#     return ans
 
 
-def solution(number, k):
-    answer = [] # Stack
+# def solution(number, k):
+#     answer = [] # Stack
     
-    for num in number:
-        if not answer:
-            answer.append(num)
-            continue
-        if k > 0:
-            while answer[-1] < num:
-                answer.pop()
-                k -= 1
-                if not answer or k <= 0:
-                    break
-        answer.append(num)
+#     for num in number:
+#         if not answer:
+#             answer.append(num)
+#             continue
+#         if k > 0:
+#             while answer[-1] < num:
+#                 answer.pop()
+#                 k -= 1
+#                 if not answer or k <= 0:
+#                     break
+#         answer.append(num)
         
-    answer = answer[:-k] if k > 0 else answer
-    return ''.join(answer)
+#     answer = answer[:-k] if k > 0 else answer
+#     return ''.join(answer)
 # number = "1231234"
     # while 1:
     #     grdval = "0"
@@ -78,11 +78,41 @@ def solution(number, k):
    
 
 # number= "1924"
-number = "1231234"
-# print(len(number))
-# number = [[j,i] for i,j in enumerate(number)]
-print(solution(number, 3))
+# number = "1231234"
+# # print(len(number))
+# # number = [[j,i] for i,j in enumerate(number)]
+# print(solution(number, 3))
 
 # a= [[1,0],[5,1],[4,2],[9,3],[5,4]]
 # print(list(permutations(a,2)))
 # print(list(product(a, repeat=2)))
+
+
+
+def solution(number, k):
+    # return max(["".join(i) for i in list(combinations(number,len(number)-k))])
+    lst = list(number)
+    
+    answer = ""
+    remain_len = len(lst)- k
+    i, j = 0, 0 - remain_len + 1
+    while 1:
+        # print(lst[i:j])
+        if j == 0:
+            j = len(number)
+        if lst[i:j]:
+            answer += max(lst[i:j])
+        else:
+            return answer
+        remain_len -= 1
+        if len(answer) == len(number) - k:
+            return answer
+        i += lst[i:j].index(answer[-1])+1
+        j = 0 - remain_len + 1
+        
+
+
+# print(solution("4177252841",4))
+print(solution("6547", 3))
+
+# print("4177252841"[8:10])
