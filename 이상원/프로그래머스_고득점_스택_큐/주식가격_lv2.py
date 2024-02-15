a0554732848f133128da9acf4e5746aa9365b193
knowmetoowell <87797445+knowmetoowell@users.noreply.github.com>
2024-02-15 10:30:41 +0900

    Update 주식가격_lv2.py

--- 이상원/프로그래머스_고득점_스택_큐/주식가격_lv2.py
+++ 이상원/프로그래머스_고득점_스택_큐/주식가격_lv2.py
@@ -1,18 +1,35 @@
-def solution(prices):
-    answer = [0]*len(prices)
-    count = 0
-    for i in range(len(prices)):
-        count = 0
-        for j in range(i+1, len(prices)):
-            if prices[i] <= prices[j]:
-                count+=1
-            else:
-                count+=1
-                break
-        answer[i] = count
+# def solution(prices):
+#     answer = [0]*len(prices)
+#     count = 0
+#     for i in range(len(prices)):
+#         count = 0
+#         for j in range(i+1, len(prices)):
+#             if prices[i] <= prices[j]:
+#                 count+=1
+#             else:
+#                 count+=1
+#                 break
+#         answer[i] = count
 
             
+#     return answer
+
+
+# print(solution([1, 2, 3, 2, 3]))
+
+
+def solution(prices):
+    answer = [0]*len(prices)
+    stack = []
+    for i in range(len(prices)):
+        while stack and prices[stack[-1]] > prices[i]:
+            temp = stack.pop()
+            answer[temp] = i-temp
+        stack.append(i)
+
+    while stack:
+        temp = stack.pop()
+        answer[temp] = len(prices)-1-temp 
     return answer
-
-
 print(solution([1, 2, 3, 2, 3]))
+
